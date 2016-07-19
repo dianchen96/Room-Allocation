9f747649a38df76438aa04c4f6ef12345d922c8c
Dian Chen <dianchen96@gmail.com>
2016-07-19 00:10:05 -0700

    modified wsgi.py

--- roomalloc/wsgi.py
+++ roomalloc/wsgi.py
@@ -7,12 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
+import sys
+import time
+import traceback
+import signal
 
 sys.path.insert(0, '/opt/python/current/app')
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'roomalloc.settings'
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roomalloc.settings")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
