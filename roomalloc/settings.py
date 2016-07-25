3c7228010eca60040e7c871ad22bccad05f88a16
Dian Chen <dianchen96@gmail.com>
2016-07-25 13:48:11 -0700

    deploy config

--- roomalloc/settings.py
+++ roomalloc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^ikr+g4eebhu*x+nxc*+rbbyvtx$%ke-&e67&t5e_49)3h@c0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
